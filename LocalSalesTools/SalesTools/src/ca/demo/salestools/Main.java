27494b70bc99faa2354f05ac786430d18f1b33c6
Åke <gitslask@hig.se>
2016-01-28 14:43:53 +0100

    Initial commit for salesTools

---
+++ LocalSalesTools/SalesTools/src/ca/demo/salestools/Main.java
@@ -0,0 +1,18 @@
+package ca.demo.salestools;
+
+public class Main {
+
+	public static void main(String[] args) {
+		SalesData data = new SalesData();
+		
+		displayGreeting();
+		data.display();
+
+	}
+	
+	private static void displayGreeting(){
+		System.out.println("HELLO HAPPY SALES PERSONS!");
+		System.out.println("THIS APP SHOWS SALES DATA");
+	}
+
+}
